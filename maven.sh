f53513764c5f371f9f41d730c408d4a21b70843f
NagarajKamath <71336337+NagarajKamath@users.noreply.github.com>
2024-08-03 17:14:43 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
+original_dir=$(pwd)
+echo "Checking and installing necessary packages. This may take sometimes..."
 # Update and upgrade packages
-sudo apt update
-sudo apt upgrade -y
+sudo apt update  &> /dev/null
+sudo apt upgrade -y  &> /dev/null
 
 # Install OpenJDK 17
-sudo apt install openjdk-17-jdk -y
+# Check if Java 17 or above is already installed
+JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+REQUIRED_VERSION="17"
 
-# Verify Java version
-java -version
+if [[ "$JAVA_VERSION" < "$REQUIRED_VERSION" ]]; then
+    echo "Java version is below $REQUIRED_VERSION. Installing OpenJDK 17..."
+    sudo apt install openjdk-17-jdk -y  &> /dev/null
+else
+    echo "Java version $JAVA_VERSION is already installed."
+fi
+
+# Set Java 17 as the default version
+sudo update-alternatives --set java /usr/lib/jvm/java-17-openjdk-amd64/bin/java  &> /dev/null
 
 # Download and extract Apache Maven
-wget https://archive.apache.org/dist/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
-sudo tar xf apache-maven-3.9.6-bin.tar.gz -C /opt
+wget https://archive.apache.org/dist/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz  &> /dev/null
+sudo tar xf apache-maven-3.9.6-bin.tar.gz -C /opt  &> /dev/null
 
 # Create a symbolic link for Maven
-sudo ln -s /opt/apache-maven-3.9.6 /opt/maven
+sudo ln -s /opt/apache-maven-3.9.6 /opt/maven  &> /dev/null
 
 # Configure environment variables
-sudo tee /etc/profile.d/maven.sh <<EOF
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-export M2_HOME=/opt/maven
-export MAVEN_HOME=/opt/maven
-export PATH=\${M2_HOME}/bin:\${PATH}
-EOF
+sudo cp "$original_dir/env_var" /etc/profile.d/maven.sh 
 
 # Make the script executable
 sudo chmod +x /etc/profile.d/maven.sh
